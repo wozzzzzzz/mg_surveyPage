a01477ed337ee2430cf03ecad85157897045b8cb
wozzzzzzz <wonderhy11@gmail.com>
2024-06-02 11:11:19 +0900

    Updates

---
+++ precache-manifest.ae211e91c50bfd8573b45c7ae833dfe3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db4f9448d957e09960109f8a8ece288a",
+    "url": "/mg_surveyPage/index.html"
+  },
+  {
+    "revision": "875ee85738571f46d557",
+    "url": "/mg_surveyPage/static/css/main.3de724d3.chunk.css"
+  },
+  {
+    "revision": "66d74e25c05f76818016",
+    "url": "/mg_surveyPage/static/js/2.15b5c64c.chunk.js"
+  },
+  {
+    "revision": "875ee85738571f46d557",
+    "url": "/mg_surveyPage/static/js/main.7fa3ab8d.chunk.js"
+  },
+  {
+    "revision": "ed2839fde738de04f192",
+    "url": "/mg_surveyPage/static/js/runtime~main.5b7d56df.js"
+  },
+  {
+    "revision": "9e6ddbb487bc1cf4119b3cc1d3ea4b49",
+    "url": "/mg_surveyPage/static/media/굴레.9e6ddbb4.png"
+  },
+  {
+    "revision": "e885f70b71e934aa8b518e3f8704d2b6",
+    "url": "/mg_surveyPage/static/media/망개.e885f70b.png"
+  },
+  {
+    "revision": "d85faffd5defb4f6c36c021a5bd1906d",
+    "url": "/mg_surveyPage/static/media/밥알.d85faffd.png"
+  },
+  {
+    "revision": "78e77c18176b41773eaf620dbd752a98",
+    "url": "/mg_surveyPage/static/media/사과.78e77c18.png"
+  },
+  {
+    "revision": "9bfa3f876ba754f139ae79e1e0df4b54",
+    "url": "/mg_surveyPage/static/media/수제.9bfa3f87.png"
+  },
+  {
+    "revision": "1a38d46a2d5cf70312d104c3cc34d310",
+    "url": "/mg_surveyPage/static/media/오메기.1a38d46a.png"
+  },
+  {
+    "revision": "4693c29e66ee6d4480cf30aa86cfc358",
+    "url": "/mg_surveyPage/static/media/흑임자.4693c29e.png"
+  }
+]);
